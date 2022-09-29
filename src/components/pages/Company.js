b334f92fc3967b085ee59b1cd6ea2628df6806d5
Vinicius Borba Queiroz <borbaqueirozvinicius@gmail.com>
2022-09-28 22:26:04 -0300

    Projeto React CalPro

---
+++ src/components/pages/Company.js
@@ -0,0 +1,19 @@
+import styles from './Company.module.css';
+function Company() {
+  return (
+    <section>
+      <h1 className={styles.style}>Sobre a empresa</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Perspiciatis
+        rerum, doloremque accusamus alias eos, qui itaque similique adipisci,
+        sequi sed quasi minus! Perspiciatis voluptatibus atque ipsam deleniti
+        iste, dolorem porro! Lorem ipsum dolor sit amet consectetur, adipisicing
+        elit. Perspiciatis rerum, doloremque accusamus alias eos, qui itaque
+        similique adipisci, sequi sed quasi minus! Perspiciatis voluptatibus
+        atque ipsam deleniti iste, dolorem porro!
+      </p>
+    </section>
+  );
+}
+
+export default Company;
---
+++ src/components/pages/Contact.js
@@ -0,0 +1,17 @@
+import styles from './Contact.module.css';
+
+function Contact() {
+  return (
+    <section>
+      <h1 className={styles.p}>Contato</h1>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptas
+        officiis molestiae fugiat tempora quod. Nulla ullam alias illum
+        repellendus delectus, cumque voluptatem eveniet quam explicabo libero
+        ut! Ut, autem quidem?
+      </p>
+    </section>
+  );
+}
+
+export default Contact;
